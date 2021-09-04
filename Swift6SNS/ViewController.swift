807e8911c371e1e41e6d8f37ae9a9042cb025db3
Daigo Kondo <daigo.soccer.0106@icloud.com>
2021-09-04 23:47:58 +0900

    Initial Commit

---
+++ Swift6SNS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift6SNS
+//
+//  Created by 近藤大伍 on 2021/09/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
